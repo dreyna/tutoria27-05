33e568d8a285b0cbf9afa6fed860f37c92b5948f
dreyna <reyna_b@upeu.edu.pe>
2021-06-27 20:11:09 -0500

    v1

---
+++ src/main/java/pe/edu/upeu/tutoria27_06/config/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.upeu.tutoria27_06.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author dreyna
+ */
+public class Conexion {
+
+    private static final String URL = "jdbc:oracle:thin:@localhost:1522:XE";
+    private static final String USER = "lp2";
+    private static final String PASS = "1234";
+    private static final String DRIVER = "oracle.jdbc.OracleDriver";
+    private static Connection cx = null;
+
+    public static Connection getConexion() {
+        try {
+            Class.forName(DRIVER);
+            if (cx == null) {
+                cx = DriverManager.getConnection(URL, USER, PASS);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO: handle exception
+            System.out.println("Error: " + e);
+        }
+
+        return cx;
+    }
+
+    public void desconectar() {
+        try {
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
---
+++ src/main/java/pe/edu/upeu/tutoria27_06/daoImpl/UsuarioDaoImpl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.upeu.tutoria27_06.daoImpl;
+
+import pe.edu.upeu.tutoria27_06.dao.UsuarioDao;
+import pe.edu.upeu.tutoria27_06.model.Usuario;
+
+/**
+ *
+ * @author dreyna
+ */
+public class UsuarioDaoImpl implements UsuarioDao {
+
+    @Override
+    public int create(Usuario u) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
